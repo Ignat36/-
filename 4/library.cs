7ea138fe9645974a95cc0b52b0be2ddb8de7b642
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-04-02 00:18:41 +0300

    Create library.cs

---
+++ 4/library.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UtilityLibraries
+{
+    public static class MyLibrary
+    {
+        public static unsafe void Swap(int *x, int *y)
+        {
+            int tmp = *x;
+            *x = *y;
+            *y = tmp;
+        }
+
+        public static unsafe void Inc(int *x)
+        {
+            (*x)++;
+        }
+
+        public static unsafe int Sum(int* x, int *y)
+        {
+            return *x + *y;
+        }
+    }
+}
