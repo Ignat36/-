d77183a18036fc3a2289e6d88a983a351f82a56a
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-04-02 13:30:30 +0300

    Create demonstration.cs

---
+++ 4/demonstration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UtilityLibraries;
+
+class Program
+{
+    static unsafe void Main(string[] args)
+    {
+        int x, y;
+        x = 5;
+        y = 6;
+
+        MyLibrary.Swap(&x, &y);
+        MyLibrary.Inc(&x);
+
+        Console.WriteLine("x = {0},  y = {1}", x, y);
+    }
+}
