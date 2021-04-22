b8f19b1b5280266d51666bae158b14d492d0c284
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-04-22 13:18:12 +0300

    Update Program.cs

--- 5/Program.cs
+++ 5/Program.cs
@@ -310,7 +310,12 @@ namespace Cars
     }
     sealed class McLaren : Car
     {
-
+        public McLaren(string CarModel, int Weight, int MaxWeight, int PassengerSeats, int TankCapacity, int TrunkVolume, double FuelFlow, int MaxSpeed) :
+            base(Weight, MaxWeight, PassengerSeats, TankCapacity, TrunkVolume, FuelFlow, MaxSpeed)
+        {
+            Brand = "McLaren";
+            Model = CarModel;
+        }
     }
     class Program
     {
