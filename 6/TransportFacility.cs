f13275a3f99f0998d52f95c5e1355fbe1616df69
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-04-29 00:02:14 +0300

    Create TransportFacility.cs

---
+++ 6/TransportFacility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    abstract class TransportFacility
+    {
+        protected int Weight, MaxWeight;
+        protected int PassengerSeats;
+        protected int MaxSpeed;
+        protected double MaxDistance;
+    }
+}
