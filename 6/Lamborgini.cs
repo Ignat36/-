e82ece9cfbc9eeb578e9e47d1cb0652cdc08abbd
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-04-29 00:02:23 +0300

    Create Lamborgini.cs

---
+++ 6/Lamborgini.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    sealed class Lamborgini : Car
+    {
+        enum Mode { ON, OFF }
+        private Mode SportiveMode;
+        public void SportON()
+        {
+            if (SportiveMode == Mode.OFF)
+            {
+                MaxSpeed = (int)(MaxSpeed * 1.5);
+                SportiveMode = Mode.ON;
+            }
+            else
+                Console.WriteLine("Sportive mode is alrady ON!");
+        }
+        public void SportOFF()
+        {
+            if (SportiveMode == Mode.ON)
+            {
+                MaxSpeed = (int)(MaxSpeed / 1.5);
+                SportiveMode = Mode.OFF;
+            }
+            else
+                Console.WriteLine("Sportive mode is alrady ON!");
+        }
+        public Lamborgini(string CarModel, int Weight, int MaxWeight, int PassengerSeats, int TankCapacity, int TrunkVolume, double FuelFlow, int MaxSpeed) :
+            base(Weight, MaxWeight, PassengerSeats, TankCapacity, TrunkVolume, FuelFlow, MaxSpeed)
+        {
+            Brand = "Lamborgini";
+            Model = CarModel;
+            SportiveMode = Mode.OFF;
+        }
+    }
+}
