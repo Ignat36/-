a3a2d247084c0810ebdd939e85669e8b14431225
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-04-29 00:02:26 +0300

    Create Item.cs

---
+++ 6/Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Item
+    {
+        private string ItemName;
+        private double ItemVolume;
+        private bool NullItemFlag;
+
+        public string Name
+        {
+            get
+            {
+                return ItemName;
+            }
+        }
+        public double Volume
+        {
+            get
+            {
+                return ItemVolume;
+            }
+        }
+        public Item(string name, double volume = 0.0001)
+        {
+            name.Clone();
+            ItemName = name;
+            ItemVolume = volume;
+            NullItemFlag = false;
+        }
+        public Item(string name, double height, double length, double width)
+        {
+            name.Clone();
+            ItemName = name;
+            ItemVolume = height * length * width;
+            NullItemFlag = false;
+        }
+
+        public Item() { NullItemFlag = true; }
+
+        public bool IsNull
+        {
+            get { return NullItemFlag; }
+        }
+    }
+}
