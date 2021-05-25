7d4c85fbc845b12ff9aee12d6511443ca142ae12
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-05-25 10:22:56 +0300

    Create CarCompare.cs

---
+++ 8/CarCompare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class CarCompare : IComparer<Car>
+    {
+
+        public int Compare(Car first, Car second)
+        {
+            double value1 = first.MSpeed / 400.0 + first.MDistance / 1000.0;
+            double value2 = second.MSpeed / 400.0 + second.MDistance / 1000.0;
+
+            if (value1 > value2)
+                return 1;
+            else
+                return 2;
+        }
+
+    }
+}
