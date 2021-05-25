51b54ed93cd274ea3680e53d10321123cbd61165
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-05-25 10:23:05 +0300

    Create ICastomizableCar.cs

---
+++ 8/ICastomizableCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public interface ICastomizableCar
+    {
+        public void setCastomColor(int[,] arr);
+        public void NeonColor(int color);
+        public void setEngine(int speed, int capacity);
+        public void setSaspension(int height, int KHidraulicCompression);
+        public void setWindowColor(int color);
+    }
+}
