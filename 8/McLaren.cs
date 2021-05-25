39993ca8075f2def24b621152bb472d761b8acb6
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-05-25 10:23:16 +0300

    Create McLaren.cs

---
+++ 8/McLaren.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    sealed class McLaren : Car
+    {
+        public McLaren(string CarModel, int Weight, int MaxWeight, int PassengerSeats, int TankCapacity, int TrunkVolume, double FuelFlow, int MaxSpeed) :
+            base(Weight, MaxWeight, PassengerSeats, TankCapacity, TrunkVolume, FuelFlow, MaxSpeed)
+        {
+            Brand = "McLaren";
+            Model = CarModel;
+        }
+    }
+}
